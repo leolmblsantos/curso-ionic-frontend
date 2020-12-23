51eab0d4b1456d0f63c098400e2c5bab294dbd3b
Leonardo Santos <leolmblsantos@gmail.com>
2020-12-23 09:32:23 -0300

    Salvando o email do token e armazenando o em localStorage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, MenuController, NavController } from 'ionic-angular';
+import { NavController, IonicPage } from 'ionic-angular';
+import { MenuController } from 'ionic-angular/components/app/menu-controller';
 import { CredenciaisDTO } from '../../models/credenciais.dto';
 import { AuthService } from '../../services/auth.service';
 
@@ -32,7 +33,7 @@ export class HomePage {
   login(){
     this.auth.authenticate(this.creds)
     .subscribe(response => {
-      this.auth.successfullogin(response.headers.get('Authorization'));
+      this.auth.successfulLogin(response.headers.get('Authorization'));
       this.navCtrl.setRoot('CategoriasPage');
     },
     error => {});
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,40 +1,40 @@
-import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { API_CONFIG } from "../config/api.config";
 import { CredenciaisDTO } from "../models/credenciais.dto";
-import { Observable } from 'rxjs/Rx'; // IMPORTANTE: IMPORT ATUALIZADO
+import { HttpClient } from "@angular/common/http";
+import { API_CONFIG } from "../config/api.config";
 import { LocalUser } from "../models/local_user";
 import { StorageService } from "./storage.service";
+import { JwtHelper } from 'angular2-jwt';
+
 
 @Injectable()
-export class AuthService{
+export class AuthService {
 
+    jwtHelper: JwtHelper = new JwtHelper();
 
-        constructor(public http: HttpClient, public storage: StorageService) {
-
-        }
-
-    authenticate(creds : CredenciaisDTO) {
-       return this.http.post(`${API_CONFIG.baseUrl}/login`,
-         creds,
-        {
-            observe: 'response',
-            responseType: 'text'
-        });
-
+    constructor(public http: HttpClient, public storage: StorageService) {
     }
 
-    successfullogin(authorizationValue : string) {
+    authenticate(creds : CredenciaisDTO) {
+        return this.http.post(
+            `${API_CONFIG.baseUrl}/login`, 
+            creds,
+            {
+                observe: 'response',
+                responseType: 'text'
+            });
+    }
+
+    successfulLogin(authorizationValue : string) {
         let tok = authorizationValue.substring(7);
         let user : LocalUser = {
-            token: tok
+            token: tok,
+            email: this.jwtHelper.decodeToken(tok).sub
         };
-
         this.storage.setLocalUser(user);
     }
 
     logout() {
         this.storage.setLocalUser(null);
     }
-    
 }
